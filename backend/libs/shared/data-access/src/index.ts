491e0305049041e01bbee99d55733b3b9f7f7852
Vladimir Skudin <skudinva@yandex.ru>
2025-02-12 15:44:08 +0700

    ++

--- backend/libs/shared/data-access/src/index.ts
+++ backend/libs/shared/data-access/src/index.ts
@@ -1,3 +1,2 @@
-export { BaseMemoryRepository } from './repository/base-memory.repository';
 export { BaseMongoRepository } from './repository/base-mongo.repository';
 export { BasePostgresRepository } from './repository/base-postgres.repository';
