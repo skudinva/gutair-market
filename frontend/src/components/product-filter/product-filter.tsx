16304ed1f2fec4088b0e18826b99e3f5934debbd
Vladimir Skudin <skudinva@yandex.ru>
2025-02-19 23:33:08 +0700

    ++

--- frontend/src/components/product-filter/product-filter.tsx
+++ frontend/src/components/product-filter/product-filter.tsx
@@ -1,75 +1,45 @@
+import { CORDS_COUNT, PRODUCT_TYPES_NAMES } from '../../const';
+
 const ProductFilter = (): JSX.Element => (
   <form className="catalog-filter" action="#" method="post">
     <h2 className="title title--bigger catalog-filter__title">Фильтр</h2>
     <fieldset className="catalog-filter__block">
       <legend className="catalog-filter__block-title">Тип гитар</legend>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="acoustic"
-          name="acoustic"
-        />
-        <label htmlFor="acoustic">Акустические гитары</label>
-      </div>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="electric"
-          name="electric"
-        />
-        <label htmlFor="electric">Электрогитары</label>
-      </div>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="ukulele"
-          name="ukulele"
-        />
-        <label htmlFor="ukulele">Укулеле</label>
-      </div>
+      {Object.entries(PRODUCT_TYPES_NAMES).map(([productType, productName]) => {
+        return (
+          <div
+            className="form-checkbox catalog-filter__block-item"
+            key={`item-type-${productType}`}
+          >
+            <input
+              className="visually-hidden"
+              type="checkbox"
+              id={productType}
+              name={productType}
+            />
+            <label htmlFor={productType}>{productName}</label>
+          </div>
+        );
+      })}
     </fieldset>
     <fieldset className="catalog-filter__block">
       <legend className="catalog-filter__block-title">Количество струн</legend>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="4-strings"
-          name="4-strings"
-        />
-        <label htmlFor="4-strings">4</label>
-      </div>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="6-strings"
-          name="6-strings"
-        />
-        <label htmlFor="6-strings">6</label>
-      </div>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="7-strings"
-          name="7-strings"
-        />
-        <label htmlFor="7-strings">7</label>
-      </div>
-      <div className="form-checkbox catalog-filter__block-item">
-        <input
-          className="visually-hidden"
-          type="checkbox"
-          id="12-strings"
-          name="12-strings"
-          disabled
-        />
-        <label htmlFor="12-strings">12</label>
-      </div>
+      {CORDS_COUNT.map((cord) => {
+        return (
+          <div
+            className="form-checkbox catalog-filter__block-item"
+            key={`string-qty-${cord}`}
+          >
+            <input
+              className="visually-hidden"
+              type="checkbox"
+              id={`${cord}-strings`}
+              name={`${cord}-strings`}
+            />
+            <label htmlFor={`${cord}-strings`}>{cord}</label>
+          </div>
+        );
+      })}
     </fieldset>
     <button
       className="catalog-filter__reset-btn button button--black-border button--medium"
