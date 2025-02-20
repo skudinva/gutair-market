0658b483a7c54e007c416bc61346e7dc52f7f612
Vladimir Skudin <skudinva@yandex.ru>
2025-02-20 13:02:41 +0700

    ++

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -1,3 +1,6 @@
+import { AppRoute } from '../../const';
+import history from '../../history';
+
 const NotFound = (): JSX.Element => (
   <section className="error">
     <h1 className="error__title">404</h1>
@@ -7,7 +10,10 @@ const NotFound = (): JSX.Element => (
       <br />
       её вовсе не существовало.
     </p>
-    <button className="button button__error button--small button--black-border">
+    <button
+      className="button button__error button--small button--black-border"
+      onClick={() => history.push(AppRoute.Root)}
+    >
       Продолжить покупки
     </button>
   </section>
